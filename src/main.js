d72477ee3975e6dd72263bd67a36322c41683b9f
Amirahyon <amirahyon@gmail.com>
2024-03-08 04:20:25 +0100

    SafeHaven

---
+++ src/main.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.menu');
+const icon = document.querySelector('.icon');
+const menuIcon = document.querySelector('.line-icon');
+const closeIcon = document.querySelector('.close');
+
+function toggleMenu() {
+    if(menu.classList.contains("show-menu")){
+        menu.classList.remove("show-menu");
+        menuIcon.style.display = 'block';
+        closeIcon.style.display = 'none';
+    } else {
+        menu.classList.add("show-menu");
+        menuIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
+    }
+}
+menu.addEventListener("click",toggleMenu)
